69fb0a315988ce7f871279b05437033bff0f19c9
21gerace <122989702+mgerace2025@users.noreply.github.com>
2023-03-01 15:46:16 -0500

    Update Diamond.java
    finished assignment 2.  I made the diamond by starting out on paper.

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
 public class Diamond {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args){
+        int number, x, y, space = 1;
+        System.out.print("Enter the number of rows for the diamond: ");
+        Scanner s = new Scanner(System.in);
+        number = s.nextInt();
+        space = number - 1;
+        for (y = 1; y <= number; y++)
+        {
+            for (x = 1; x <= space; x++)
+            {
+                System.out.print(" ");
+            }
+            space--;
+            for (x = 1; x <= 2 * y - 1; x++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        space = 1;
+        for (y = 1; y <= number - 1; y++)
+        {
+            for (x = 1; x <= space; x++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for (x = 1; x <= 2 * (number - y) - 1; x++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
